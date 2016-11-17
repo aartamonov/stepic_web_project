66ad7274801ce1e5a96c4ce3d9c1e9fb249f0592
cahek <cahek@some.mail>
2016-11-17 15:56:39 +0000

    task 1.9.11 final

--- etc/hello.py
+++ etc/hello.py
@@ -1,6 +1,6 @@
 CONFIG = {
 	'mode': 'wsgi',
-	'working_dir': '/home/cahek/webProg/web',
+	'working_dir': '/home/box/web',
 	'args': (
 		'--bind=0.0.0.0:8080',
 		'--daemon',
--- hello.py
+++ hello.py
@@ -5,7 +5,12 @@ def app(environ, start_response):
 	]
 	qs = environ['QUERY_STRING']	
 	param_list = qs.split('&')
-	body = ['<html><body>'] + ['%s<br>' % (str(param)) for param in param_list] + ['</body></html>']
+	result = ''
+	for param in param_list:
+		result = result + param + "\n"
+	body = [result]
+#	body = ['<html><body>'] + ['%s<br>' % (str(param)) for param in param_list] + ['</body></html>']
 	
 	print body
+	start_response(status, headers);
 	return body
